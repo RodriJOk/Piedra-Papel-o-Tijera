0e3e3072e5d1233ba069bd2a5e869448b6cfce3e
Rodrigo Juarez <juarezrodrigo59@gmail.com>
2021-04-21 17:29:14 -0300

    Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import React, { createContext, useState } from 'react';
 import './App.css';
+import Header from './components/Header'
+import styled from 'styled-components'
+import Wrapper from './components/Wrapper'
+import Table from './components/Table'
+import Rules from './components/Rules'
+
+
+export const ScoreContext = createContext()
+
+const AppStyled = styled.main`
+  background-image: radial-gradient(circle at top, #1F3757 20%, #131537 100%);
+  color: white;
+  font-family: 'Barlow Semi Condensed', sans-serif;
+  .app-content {
+    padding: 2em;
+    min-height: 100vh;
+    display: flex;
+    box-sizing: border-box;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+`
 
 function App() {
+  const [score, setScore] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ScoreContext.Provider value={{
+      score,
+      setScore,
+    }}>
+      <AppStyled>
+        <Wrapper>
+          <div className="app-content">
+            <Header />
+            <Table />
+            <Rules />
+          </div>
+        </Wrapper>
+      </AppStyled>
+    </ScoreContext.Provider>
   );
 }
 
